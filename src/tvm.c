5927b63b614d5f6a8a582bd60d0b1010eb01b9f4
Muhammed Yasinhan Yaşar <sesden19611@gmail.com>
2024-08-03 23:03:27 +0300

    Stack based VM started to implement

---
+++ src/tvm.c
@@ -0,0 +1,18 @@
+#define TVM_IMPLEMENTATION
+#include <tvm.h>
+
+opcode_t program[] = {
+    {.type = OP_PUSH, .operand = 3},
+    {.type = OP_PUSH, .operand = 5},
+    {.type = OP_ADD},
+    {.type = OP_HALT},
+};
+
+int main(int argc, char **argv) {
+    
+    tvm_t vm = tvm_init();
+    tvm_load_program_from_memory(&vm, program, 4);
+    tvm_run(&vm);
+
+    return 0;
+}
