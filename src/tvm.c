e2d8be5f952f5a8cbb401b6f6238dcf350014cdb
unknown <berkeonver@icloud.com>
2024-08-07 00:00:26 +0300

    tvm_exec_opcode completed.

--- src/tvm.c
+++ src/tvm.c
@@ -2,10 +2,17 @@
 #include <tvm/tvm.h>
 
 opcode_t program[] = {
-    {.type = OP_PUSH, .operand = 3},
-    {.type = OP_PUSH, .operand = 5},
-    {.type = OP_ADD},
-    {.type = OP_HALT},
+    {.type = OP_PUSH, .operand.i32 = 1}, 
+    {.type = OP_PUSH, .operand.i32 = 2},  
+    {.type = OP_PUSH, .operand.i32 = 3},
+    {.type = OP_JNZ, .operand.i32 = 11},                 
+    {.type = OP_PUSH, .operand.i32 = 4},
+    {.type = OP_PUSH, .operand.i32 = 5},
+    {.type = OP_PUSH, .operand.i32 = 6},
+    {.type = OP_PUSH, .operand.i32 = 7},
+    {.type = OP_PUSH, .operand.i32 = 8},
+    {.type = OP_PUSH, .operand.i32 = 9},
+    {.type = OP_HALT}                   
 };
 
 int main(int argc, char **argv) {
@@ -13,7 +20,7 @@ int main(int argc, char **argv) {
     UNUSED_VAR(argv);
     
     tvm_t vm = tvm_init();
-    tvm_load_program_from_memory(&vm, program, 4);
+    tvm_load_program_from_memory(&vm, program, 9);
     tvm_run(&vm);
 
     return 0;
