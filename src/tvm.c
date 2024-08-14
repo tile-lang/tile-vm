a9f8c63b3debef709df11cbd0ef2bce4e4e4703e
Mehmet Can <104527797+MehmetJohn@users.noreply.github.com>
2024-08-14 20:21:10 +0300

    File read added

--- src/tvm.c
+++ src/tvm.c
@@ -2,25 +2,22 @@
 #include <tvm/tvm.h>
 
 opcode_t program[] = {
-    {.type = OP_PUSH, .operand.i32 = 16},
-    {.type = OP_PUSH, .operand.f32 = 2.4},
-    {.type = OP_DECF},
-    {.type = OP_PUSH, .operand.f32 = -7.89},
-    {.type = OP_INCF},
-    {.type = OP_PUSH, .operand.i32 = -11},
-    {.type = OP_INC},
-    {.type = OP_PUSH, .operand.i32 = -85},
-    {.type = OP_DEC},
-    {.type = OP_PUSH, .operand.f32 = 3.4},
-    {.type = OP_HALT}
+    // Main Program
+    {.type = OP_PUSH, .operand.i32 = 4},
+    {.type = OP_PUSH, .operand.i32 = 2}, 
+    {.type = OP_HALT},               
 };
+
+
 
 int main(int argc, char **argv) {
     UNUSED_VAR(argc);
     UNUSED_VAR(argv);
     
     tvm_t vm = tvm_init();
-    tvm_load_program_from_memory(&vm, program, 9);
+    //tvm_load_program_from_memory(&vm, program, 15);
+    //const char *file_path = "out.bin";
+    tvm_load_program_from_file(&vm, "out.bin");
     tvm_run(&vm);
 
     return 0;
