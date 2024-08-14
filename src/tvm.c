481278b05575c0bb29db8a7592df4665b09ae721
unknown <berkeonver@icloud.com>
2024-08-14 13:30:44 +0300

    CLN and SWAP added

--- src/tvm.c
+++ src/tvm.c
@@ -4,8 +4,13 @@
 opcode_t program[] = {
     // Main Program
     {.type = OP_PUSH, .operand.i32 = 4},
-    {.type = OP_PUSH, .operand.i32 = 2}, 
-    {.type = OP_HALT},               
+    {.type = OP_PUSH, .operand.ui32 = 2},
+    {.type = OP_PUSH, .operand.f32 = 3.4},
+    {.type = OP_PUSH, .operand.f32 = 7.4},
+    {.type = OP_PUSH, .operand.i32 = 6},
+    {.type = OP_PUSH, .operand.f32 = 8.4},
+    {.type = OP_CLN, .operand.i32 = 12},
+    {.type = OP_HALT}               
 };
 
 
@@ -15,9 +20,9 @@ int main(int argc, char **argv) {
     UNUSED_VAR(argv);
     
     tvm_t vm = tvm_init();
-    //tvm_load_program_from_memory(&vm, program, 15);
+    tvm_load_program_from_memory(&vm, program, 15);
     //const char *file_path = "out.bin";
-    tvm_load_program_from_file(&vm, "out.bin");
+    //tvm_load_program_from_file(&vm, "out.bin");
     tvm_run(&vm);
 
     return 0;
